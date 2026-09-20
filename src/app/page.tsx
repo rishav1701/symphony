ac23ff973cd611047fe4aa67ed1fb950de365746
rishav1701 <rixchu458@gmail.com>
2026-09-20 21:22:44 +0530

    refactor: remove VenuePolicies component from HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Hero } from "@/components/home/Hero";
 import { VenueStats } from "@/components/home/VenueStats";
-import { VenuePolicies } from "@/components/home/VenuePolicies";
 import { StorySection } from "@/components/home/StorySection";
 import { Specifications } from "@/components/shared/Specifications";
 import { ServicesTeaser } from "@/components/home/ServicesTeaser";
@@ -11,7 +10,6 @@ export default function HomePage() {
     <>
       <Hero />
       <VenueStats />
-      <VenuePolicies />
       <StorySection />
       <Specifications />
       <ServicesTeaser />
